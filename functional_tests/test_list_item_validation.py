060dc184d5271b7a1158a2b6722597f8aa3c56f9
april15th <april15th@gmail.com>
2018-08-06 14:28:13 +0900

    리스트 뷰에서 모델 유효성 검증 구현

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -9,11 +9,12 @@ class ItemValidationTest(FunctionalTest):
         # 입력 상자가 비어있는 상태에서 엔터키를 누른다. 
 
         self.browser.get(self.server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
         self.browser.find_element_by_id('id_new_item').send_keys("\n")
 
         # 페이지가 새로고침되고, 빈 아이템을 등록할 수 없다는 에러 메시지가 표시된다. 
-        error = self.browser.find_element_by_css_selector('.has_error')
-        self.assertEqual(error.text, "빈 아이템을 등록할 수 없습니다")
+        error = self.browser.find_element_by_css_selector('.has-error')
+        self.assertEqual(error.text, "You can't have an empty list item")
 
         # 다른 아이템을 입력하고 이번에는 정상 처리된다. 
         self.browser.find_element_by_id('id_new_item').send_keys("우유 사기\n")
@@ -25,9 +26,9 @@ class ItemValidationTest(FunctionalTest):
         # 리스트 페이지에 다시 에러 메시지가 표시된다
         self.check_for_row_in_list_table('1: 우유 사기')
         error = self.browser.find_element_by_css_selector('.has-error')
-        self.assertEqual(error.text, "빈 아이템을 등록할 수 없습니다")
+        self.assertEqual(error.text, "You can't have an empty list item")
 
         # 아이템을 입력하면 정상 동작한다. 
         self.browser.find_element_by_id('id_new_item').send_keys('tea 만들기\n')
-        self.check_for_row_in_list_table('1: 우유사기')
+        self.check_for_row_in_list_table('1: 우유 사기')
         self.check_for_row_in_list_table('2: tea 만들기')
--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -11,46 +11,6 @@ from lists.models import Item, List
 from django.shortcuts import resolve_url
 
 
-"""
-class NewItemTest(TestCase):
-
-    def test_can_save_a_POST_request_to_an_existing_list(self):
-        other_list = List.objects.create()
-        correct_list = List.objects.create()
-
-        self.client.post(
-            '/lists/%d/' % (correct_list.id,),
-            data={'item_text':'기존 목록에 신규 아이템'},
-        )
-
-        self.assertEqual(Item.objects.count(), 1)
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text, '기존 목록에 신규 아이템')
-        self.assertEqual(new_item.list, correct_list)
-
-    def test_POST_redirects_to_list_view(self):
-        other_list = List.objects.create()
-        correct_list = List.objects.create()
-
-        response = self.client.post(
-            '/lists/%d/' % (correct_list.id,),
-            data={'item_text': '기존 목록에 신규 아이템'}
-        )
-        self.assertRedirects(response, '/lists/%d/' % (correct_list.id,))
-
-    def test_redirects_to_list_view(self):
-        other_list = List.objects.create()
-        correct_list = List.objects.create()
-
-        response = self.client.post(
-            '/lists/%d/add_item' % (correct_list.id,),
-            data={'item_text':'기존 목록에 신규 아이템'},
-        )
-
-        self.assertRedirects(response, '/lists/%d/' % (correct_list.id,))
-"""
-
-
 class NewListTest(TestCase):
 
     def test_saving_a_POST_request(self):
@@ -155,4 +115,14 @@ class ListViewTest(TestCase):
         )
         self.assertRedirects(response, '/lists/%d/' % (correct_list.id,))
 
+    def test_validation_errors_end_up_on_lists_page(self):
+        list_ = List.objects.create()
+        response = self.client.post(
+            '/lists/%d/' % (list_.id,),
+            data={'item_text':''},
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'list.html')
+        expected_error = escape("You can't have an empty list item")
+        self.assertContains(response, expected_error)
 
