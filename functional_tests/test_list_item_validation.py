7911574b0b3d43467b73a821ed1c7e7ecc13af6f
april15th <april15th@gmail.com>
2018-08-07 11:15:06 +0900

    모든 뷰에서 폼 사용하기. 동작 상태로 복귀

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -13,8 +13,9 @@ class ItemValidationTest(FunctionalTest):
         self.get_item_input_box().send_keys("\n")
 
         # 페이지가 새로고침되고, 빈 아이템을 등록할 수 없다는 에러 메시지가 표시된다. 
-        error = self.browser.find_element_by_css_selector('.has-error')
-        self.assertEqual(error.text, "You can't have an empty list item")
+        # javascript 에 의해 post 전송시도가 방지됨
+        #error = self.browser.find_element_by_css_selector('.has-error')
+        #self.assertEqual(error.text, "You can't have an empty list item")
 
         # 다른 아이템을 입력하고 이번에는 정상 처리된다. 
         self.get_item_input_box().send_keys("우유 사기\n")
@@ -25,8 +26,8 @@ class ItemValidationTest(FunctionalTest):
 
         # 리스트 페이지에 다시 에러 메시지가 표시된다
         self.check_for_row_in_list_table('1: 우유 사기')
-        error = self.browser.find_element_by_css_selector('.has-error')
-        self.assertEqual(error.text, "You can't have an empty list item")
+        #error = self.browser.find_element_by_css_selector('.has-error')
+        #self.assertEqual(error.text, "You can't have an empty list item")
 
         # 아이템을 입력하면 정상 동작한다. 
         self.get_item_input_box().send_keys('tea 만들기\n')
