2dad8475ddb37308f0ffb7b5615a159fcd6976ad
april15th <april15th@gmail.com>
2018-08-09 09:35:00 +0900

    자바스크립트 시도 처번째

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -9,9 +9,11 @@ from django.utils.html import escape
 from lists.models import Item, List
 
 from django.shortcuts import resolve_url
-from lists.forms import ItemForm, EMPTY_LIST_ERROR
 
 from unittest import skip
+from lists.forms import (
+    DUPLICATE_ITEM_ERROR, EMPTY_LIST_ERROR, ExistingListItemForm, ItemForm,
+)
 
 
 class NewListTest(TestCase):
@@ -146,7 +148,7 @@ class ListViewTest(TestCase):
         list_ = List.objects.create()
         response = self.client.get('/lists/%d/' % (list_.id,))
 
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
         self.assertContains(response, 'name="text"')
 
     def post_invalid_input(self):
@@ -167,26 +169,22 @@ class ListViewTest(TestCase):
 
     def test_for_invalid_input_passes_form_to_template(self):
         response = self.post_invalid_input()
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
 
     def test_for_invalid_input_shows_error_on_page(self):
         response = self.post_invalid_input()
         self.assertContains(response, escape(EMPTY_LIST_ERROR))
 
-    @skip
     def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
         list1 = List.objects.create()
         item1 = Item.objects.create(list=list1, text="textey") 
-
         response = self.client.post(
             '/lists/%d/' % (list1.id,),
             data={'text':'textey'},
         )
-        print(response.contents)
-        #response = self.client.post(
-        #    '/lists/%d/' % (list_.id,),
-        #    data={'text':'sample'},
-        #)
-        #print(response)
+        expected_error = escape(DUPLICATE_ITEM_ERROR)
 
+        self.assertContains(response, expected_error)
+        self.assertTemplateUsed(response, 'list.html')
+        self.assertEqual(Item.objects.all().count(), 1)
 
